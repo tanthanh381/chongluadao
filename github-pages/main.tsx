cba8510174c9a5d3b938703137e20287e9125f53
tanthanh381 <tanthanh381@gmail.com>
2026-09-13 12:00:52 +0700

    feat: mount UX refresh layer

--- github-pages/main.tsx
+++ github-pages/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "../app/page";
+import { UxRefresh } from "../app/ux-refresh";
 import "../app/globals.css";
+import "../app/ux-refresh.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <UxRefresh />
   </StrictMode>,
 );
